0d8ccd2c843c1ecd8f2091f9914a06dea508dd1f
ThanhLy1 <123609101+ThanhLy1@users.noreply.github.com>
2023-10-29 17:16:07 -1000

    Added Askus Question and Answer to db

---
+++ app/imports/api/askus/Askus.js
@@ -0,0 +1,26 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+class AskusCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'AskusCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      filename: String,
+      question: String,
+      article_text: String,
+      // Add more fields if necessary
+    });
+    // Attach the schema to the collection.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+// The singleton instance of the AskusCollection.
+export const Askus = new AskusCollection();
