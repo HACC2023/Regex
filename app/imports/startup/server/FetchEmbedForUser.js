27be9e8c2653e5c40ac2f8ff5e802de437ae19de
ThanhLy1 <123609101+ThanhLy1@users.noreply.github.com>
2023-10-30 20:25:24 -1000

    Chatbot and Dir clean up.
    - chatbot now display relevant article
    - display top 3 article due to rate limit

--- app/imports/startup/server/FetchEmbedForUser.js
+++ app/imports/startup/server/FetchEmbedForUser.js
@@ -45,35 +45,56 @@ async function getEmbeddingFromOpenAI(text) {
 function findMostSimilarArticles(userEmbedding) {
   const articles = Askus.collection.find({}).fetch();
 
-  return articles
-    .filter((article) => Array.isArray(article.embedding))
-    .map((article) => {
-      const similarity = computeCosineSimilarity(userEmbedding, article.embedding);
-      return {
-        ...article,
-        similarity,
-      };
-    })
-    .sort((a, b) => b.similarity - a.similarity)
-    .slice(0, 5);
+  const similarities = articles.map(article => ({
+    article: article,
+    similarity: computeCosineSimilarity(userEmbedding, article.embedding),
+  }));
+
+  const sortedArticles = similarities.sort((a, b) => b.similarity - a.similarity).slice(0, 5);
+
+  return sortedArticles.map(item => item.article);
 }
 
 function getRelevantContextFromDB(userEmbedding) {
   console.log('User Embedding:', userEmbedding);
-  const similarArticles = findMostSimilarArticles(userEmbedding);
+  const similarArticles = findMostSimilarArticles(userEmbedding).slice(0, 3); // Only take top 3 articles
   console.log('Similar articles found:', similarArticles);
 
+  const maxTokensPerArticle = 600; // further reduce to 600 tokens per article
 
-  return similarArticles.map((article) => ({
+  // Truncate each article to maxTokensPerArticle tokens
+  const truncatedArticles = similarArticles.map(article => {
+    const tokens = article.article_text.split(' '); // naive tokenization by spaces
+    const truncatedText = tokens.slice(0, maxTokensPerArticle).join(' ');
+    return {
+      ...article,
+      article_text: truncatedText,
+    };
+  });
+
+  // Return messages for the chatbot
+  const messages = truncatedArticles.map((article) => ({
     role: 'system',
     content: `From ${article.filename}: ${article.article_text}`,
   }));
+
+  // Return articles in the expected format
+  const articlesForComponent = truncatedArticles.map((article) => ({
+    filename: article.filename,
+    question: article.question,
+    article_text: article.article_text,
+  }));
+
+  return {
+    messagesForChatbot: messages,
+    articlesForComponent: articlesForComponent,
+  };
 }
 
 Meteor.methods({
   async getChatbotResponse(userMessage) {
     const userEmbedding = await getEmbeddingFromOpenAI(userMessage);
-    const chatbotContext = getRelevantContextFromDB(userEmbedding);
+    const { messagesForChatbot, articlesForComponent } = getRelevantContextFromDB(userEmbedding);
 
     const initialContext = [
       { role: 'system', content: 'You are a chatbot that can only answer questions based on the following IT articles provided and nothing else.' },
@@ -82,7 +103,7 @@ Meteor.methods({
 
     const messages = [
       ...initialContext,
-      ...chatbotContext,
+      ...messagesForChatbot,
       { role: 'user', content: `Can you answer the question: ${userMessage} based on the given IT articles?` },
     ];
 
@@ -95,10 +116,12 @@ Meteor.methods({
       });
 
       if (response && response.choices && response.choices[0]) {
-        return response.choices[0].message.content;
+        return {
+          chatbotResponse: response.choices[0].message.content,
+          similarArticles: articlesForComponent, // Return the articles in the expected format
+        };
       }
       throw new Error('Unexpected OpenAI API response format');
-
     } catch (error) {
       console.error('Error during OpenAI API call:', error);
       throw new Meteor.Error('api-error', 'Failed to get a response from the chatbot');
--- app/imports/ui/components/Chatbot.jsx
+++ app/imports/ui/components/Chatbot.jsx
@@ -6,27 +6,32 @@ const ChatBox = () => {
   const [userInput, setUserInput] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [similarArticles, setSimilarArticles] = useState([]);
 
   const handleSend = (e) => {
     e.preventDefault();
     setLoading(true);
     setChatHistory([...chatHistory, { sender: 'user', text: userInput }]);
 
-    Meteor.call('getChatbotResponse', userInput, (error, response) => {
+    Meteor.call('getChatbotResponse', userInput, (error, result) => {
       setLoading(false);
       if (!error) {
-        setChatHistory([...chatHistory, { sender: 'user', text: userInput }, { sender: 'bot', text: response }]);
+        setChatHistory([...chatHistory, { sender: 'user', text: userInput }, { sender: 'bot', text: result.chatbotResponse }]);
+        setSimilarArticles(result.similarArticles);
         setUserInput('');
       } else {
         setChatHistory([...chatHistory, { sender: 'bot', text: 'Sorry, I encountered an error. Please try again later.' }]);
       }
+      // console.log('Received from server:', result); // comment this line when done testing
+
     });
   };
 
   return (
     <Container className="mt-5">
       <Row>
-        <Col>
+        {/* Chatbot Conversation Column */}
+        <Col md={8}>
           <div className="chat-window">
             {chatHistory.map((message, index) => (
               <div key={index} className={`chat-message ${message.sender}`}>
@@ -35,20 +40,40 @@ const ChatBox = () => {
             ))}
             {loading && <Spinner animation="border" />}
           </div>
+          <Row className="mt-3">
+            <Col>
+              <Form onSubmit={handleSend}>
+                <Form.Control
+                  type="text"
+                  value={userInput}
+                  onChange={(e) => setUserInput(e.target.value)}
+                  placeholder="Ask something..."
+                />
+                <Button type="submit" className="mt-2">Send</Button>
+              </Form>
+            </Col>
+          </Row>
         </Col>
-      </Row>
-      <Row className="mt-3">
-        <Col>
-          <Form onSubmit={handleSend}>
-            <Form.Control
-              type="text"
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
-              placeholder="Ask something..."
-            />
-            <Button type="submit" className="mt-2">Send</Button>
-          </Form>
+
+        {/* Similar Articles Column */}
+        <Col md={4}>
+          <h5>Similar Articles</h5>
+          {similarArticles.map((article, index) => {
+            // Truncating the article content to 200 characters for the excerpt
+            const truncatedContent = `${article.article_text.substring(0, 200)}...`;
+
+            return (
+              <div key={index} className="card mb-3" style={{ width: '18rem' }}>
+                <div className="card-body">
+                  <h5 className="card-title">{article.question}</h5>
+                  <p className="card-text">{truncatedContent}</p>
+                  <a href={`/article_html/${article.filename}`} className="card-link" target="_blank" rel="noopener noreferrer">Read full article</a>
+                </div>
+              </div>
+            );
+          })}
         </Col>
+
       </Row>
     </Container>
   );
