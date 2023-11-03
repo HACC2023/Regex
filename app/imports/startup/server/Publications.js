ba4360e7ae08ebe9d60374ce79a0e5a23e4b8359
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-02 20:08:24 -1000

    Works now, but can be very laggy!

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { AskUs } from '../../api/askus/AskUs';
 
 // User-level publication.
 // If logged in, then publish documents owned by this user. Otherwise, publish nothing.
@@ -8,6 +9,15 @@ Meteor.publish(Stuffs.userPublicationName, function () {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Stuffs.collection.find({ owner: username });
+  }
+  return this.ready();
+});
+
+// Admin-level publication.
+// If logged in and with admin role, then publish all documents from all users. Otherwise, publish nothing.
+Meteor.publish(AskUs.adminPublicationName, function () {
+  if (Roles.userIsInRole(this.userId, 'admin')) {
+    return AskUs.collection.find();
   }
   return this.ready();
 });
