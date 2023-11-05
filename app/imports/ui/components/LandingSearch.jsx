e0c03bcfbdcccfe36b321f84dc530f9aff6ab950
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-04 21:51:07 -1000

    Implemented form and button for LandingSearch

--- app/imports/ui/components/LandingSearch.jsx
+++ app/imports/ui/components/LandingSearch.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 
 /** Renders a search bar that accepts input and redirects user to chatbot page */
-const LandingSearch = () => (
-  <Container>
-    <p>Landing Search</p>
-  </Container>
-);
+const LandingSearch = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const redirect = (e) => {
+    e.preventDefault();
+    localStorage.setItem('questionAsked', userInput);
+    window.location.assign('/chatbot');
+    return false;
+  };
+
+  return (
+    <Container>
+      <Form onSubmit={redirect} className="mt-3">
+        <div className="d-flex">
+          <Form.Control
+            type="text"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            placeholder="Have a question?"
+          />
+          <Button type="submit" className="ms-2">Ask Us</Button>
+        </div>
+      </Form>
+    </Container>
+  );
+};
 
 export default LandingSearch;
