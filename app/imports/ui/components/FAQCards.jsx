510a529dedbb3a997e330b1bc51dd236c96e3245
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-05 01:48:42 -1000

    small changes in prep for FAQ cards

--- app/imports/ui/components/FAQCards.jsx
+++ app/imports/ui/components/FAQCards.jsx
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 /** Renders the top 8 most frequently asked (answered) questions */
 const FAQCards = () => (
   <Container>
-    <p>FAQ Cards</p>
+    <h4 style={{ textAlign: 'left', marginTop: '20px' }}>Frequently Asked Questions</h4>
   </Container>
 );
 
