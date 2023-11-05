d63486b1abe64f1579b8d0bce4f5072fba856f2c
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-05 02:22:41 -1000

    Added cards that display the most frequently asked questions (articles) on landing page

--- app/imports/ui/components/FAQCards.jsx
+++ app/imports/ui/components/FAQCards.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import { Container, Row, Col } from 'react-bootstrap';
+import { AskUs } from '../../api/askus/AskUs';
+import FAQCard from './FAQCard';
+import LoadingSpinner from './LoadingSpinner';
 
 /** Renders the top 8 most frequently asked (answered) questions */
-const FAQCards = () => (
-  <Container>
-    <h4 style={{ textAlign: 'left', marginTop: '20px' }}>Frequently Asked Questions</h4>
-  </Container>
-);
+const FAQCards = () => {
+  // useTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+  const { ready, articles } = useTracker(() => {
+    // Get access to Stuff documents.
+    const subscription = Meteor.subscribe(AskUs.userPublicationName);
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Stuff documents
+    const items = AskUs.collection.find().fetch();
+    return {
+      articles: items,
+      ready: rdy,
+    };
+  }, []);
+
+  return (ready ? (
+    <Container>
+      <h4 style={{ textAlign: 'left' }}>Frequently Asked Questions</h4>
+      <Row xs={2} md={3} lg={4} className="g-4">
+        {articles.map((article, index) => (<Col key={index}><FAQCard article={article} /></Col>))}
+      </Row>
+    </Container>
+  ) : <LoadingSpinner />);
+};
 
 export default FAQCards;
