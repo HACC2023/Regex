0178e5b0238e0a50c7a73c9680e76edb86d597df
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-05 01:23:17 -1000

    finished styling the search bar

--- app/imports/ui/components/LandingSearch.jsx
+++ app/imports/ui/components/LandingSearch.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Button, Container, Form, Col, Row } from 'react-bootstrap';
 
 /** Renders a search bar that accepts input and redirects user to chatbot page */
 const LandingSearch = () => {
@@ -13,19 +13,24 @@ const LandingSearch = () => {
   };
 
   return (
-    <Container>
-      <Form onSubmit={redirect} className="mt-3">
-        <div className="d-flex">
-          <Form.Control
-            type="text"
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-            placeholder="Have a question?"
-            required
-          />
-          <Button type="submit" className="ms-2">Ask Us</Button>
-        </div>
-      </Form>
+    <Container className="landingSearchContainer px-5 pt-3 pb-5">
+      <Row>
+        <Col xs={3} />
+        <Col xs={6}>
+          <Form onSubmit={redirect} className="mt-3">
+            <div className="d-flex">
+              <Form.Control
+                type="text"
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
+                placeholder="Have a question?"
+                required
+              />
+              <Button type="submit" className="landingSearchButton ms-2">Ask Us</Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 };
