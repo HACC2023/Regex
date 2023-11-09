cc84ada37385676fa4d70601e03a586aeab78dd1
ThanhLy1 <123609101+ThanhLy1@users.noreply.github.com>
2023-11-08 14:46:09 -1000

    Askus new field has, new openai model, embedded method update

--- app/imports/startup/server/AskUsMethod.js
+++ app/imports/startup/server/AskUsMethod.js
@@ -1,67 +1,97 @@
 import { Meteor } from 'meteor/meteor';
 import OpenAIApi from 'openai';
+import crypto from 'crypto';
 import { AskUs } from '../../api/askus/AskUs.js';
 
 const openai = new OpenAIApi(process.env.OPENAI_API_KEY);
-const MAX_TOKENS = 8192; // Max tokens allowed by the model
-const PARALLEL_LIMIT = 5; // Number of articles to process in parallel
+const MAX_TOKENS = 7500; // Max tokens allowed by the model: 8192
 
-// Truncates text at the last full stop before the max token limit
-const truncateText = (text, maxTokens) => {
-  if (text.length <= maxTokens) return text;
-  let end = text.lastIndexOf('.', maxTokens);
-  if (end === -1) end = maxTokens;
-  return text.substring(0, end + 1);
-};
+/* eslint-disable no-console */
 
-// Validates the OpenAI API response
-const validateOpenAIResponse = (response) => {
-  if (!response.data || !response.data[0] || !response.data[0].embedding) {
-    throw new Error('Unexpected response format');
+// Helper function to split text into chunks
+function splitIntoChunks(text, maxTokens) {
+  const chunks = [];
+  let startIndex = 0;
+  while (startIndex < text.length) {
+    let endIndex = startIndex + maxTokens;
+    if (endIndex > text.length) {
+      endIndex = text.length;
+    }
+    chunks.push(text.substring(startIndex, endIndex));
+    startIndex = endIndex;
   }
-  return response.data[0].embedding;
-};
+  return chunks;
+}
+
+// Helper function to average embeddings
+function averageEmbeddings(embeddings) {
+  const summedEmbedding = embeddings.reduce((acc, embedding) => acc.map((value, idx) => value + embedding[idx]), new Array(embeddings[0].length).fill(0));
+  return summedEmbedding.map(value => value / embeddings.length);
+}
 
 Meteor.methods({
   generateAndStoreEmbeddings: async function () {
-    const articles = AskUs.collection.find({ embedding: { $exists: false } }).fetch();
+    // Fetch all articles that either don't have an embedding or might need an update
+    const articles = AskUs.collection.find({}).fetch();
+    const updatePromises = articles.map(async (article) => {
+      console.log(`Processing article with Filename: ${article.filename}`); // Log when processing starts
+      const articleText = article.article_text;
+      const currentTextHash = crypto.createHash('md5').update(articleText).digest('hex');
 
-    // Process articles in chunks to avoid hitting rate limits
-    for (let i = 0; i < articles.length; i += PARALLEL_LIMIT) {
-      const chunk = articles.slice(i, i + PARALLEL_LIMIT);
-      const embeddingPromises = chunk.map(async (article) => {
-        const articleText = truncateText(article.article_text, MAX_TOKENS);
+      // Check if embedding exists and if the text has changed
+      const needsUpdate = !article.embedding || article.textHash !== currentTextHash;
+
+      if (!needsUpdate) {
+        // If the article doesn't need an update, skip to the next one
+        return Promise.resolve();
+      }
+
+      // If the article text is too long, split into chunks and get embeddings for each chunk
+      const chunks = splitIntoChunks(articleText, MAX_TOKENS);
+      const embeddingsPromises = chunks.map(async (chunk, index) => {
+        console.log(`Generating embedding for chunk ${index + 1}/${chunks.length} of article with ID: ${article._id}`); // Log for each chunk
 
         try {
           const response = await openai.embeddings.create({
             model: 'text-embedding-ada-002',
-            input: articleText,
+            input: chunk,
           });
-
-          return {
-            _id: article._id,
-            embedding: validateOpenAIResponse(response),
-          };
-
+          if (!response.data || !response.data[0] || !response.data[0].embedding) {
+            console.log('Unexpected response format from OpenAI for article:', article._id);
+            return null;
+          }
+          return response.data[0].embedding;
         } catch (error) {
-          console.error('Error generating embedding for article:', article._id, error);
+          console.log('Error generating embedding for article chunk:', article._id, error);
           return null;
         }
       });
 
-      // Wait for all the embeddings in the current chunk
-      const embeddings = await Promise.all(embeddingPromises);
-
-      // Bulk update the database with the new embeddings
-      embeddings.forEach((embeddingInfo) => {
-        if (embeddingInfo) {
-          AskUs.collection.update(embeddingInfo._id, {
-            $set: {
-              embedding: embeddingInfo.embedding,
-            },
-          });
+      try {
+        const chunkEmbeddings = await Promise.all(embeddingsPromises);
+        const filteredEmbeddings = chunkEmbeddings.filter(e => e !== null);
+        if (filteredEmbeddings.length === 0) {
+          throw new Error('No embeddings were generated.');
         }
-      });
-    }
+        const combinedEmbedding = averageEmbeddings(filteredEmbeddings);
+        console.log(`Updating embedding for article with Filename: ${article.filename} and ID: ${article._id}`);
+
+        // Update the article with the new embedding and hash
+        return AskUs.collection.update(article._id, {
+          $set: {
+            embedding: combinedEmbedding,
+            textHash: currentTextHash,
+          },
+        });
+
+      } catch (error) {
+        console.log('Error generating or combining embeddings for article:', article._id, error);
+        return null;
+      }
+    });
+
+    // Wait for all the update operations to complete
+    await Promise.all(updatePromises);
+    console.log('All embeddings have been processed.'); // Log when all articles have been processed
   },
 });
