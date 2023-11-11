89719159d3187cf0d8380f1b43e8820ce799be62
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-11 02:13:50 -1000

    added nice box to check embedding status, edited admin page elements

--- app/imports/ui/components/EmbeddedButton.jsx
+++ app/imports/ui/components/EmbeddedButton.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { Meteor } from 'meteor/meteor';
 
-const EmbeddingButton = (props) => {
-  const page = props;
+const EmbeddingButton = () => {
   const [loading, setLoading] = useState(false);
-  const [complete, setComplete] = useState(false);
 
   const handleGenerateEmbeddings = () => {
     setLoading(true);
@@ -14,30 +12,16 @@ const EmbeddingButton = (props) => {
       if (err) {
         alert('Error:', err.reason);
       } else {
-        setComplete(true);
         alert('Embeddings generated and stored successfully');
       }
       setLoading(false);
     });
   };
 
-  useEffect(() => {
-    if (page.props.embedding) {
-      setComplete(true);
-    }
-  }, []);
-
   return (
-    <Row sm={3}>
-      <Col>
-        <Button variant="secondary" onClick={handleGenerateEmbeddings} disabled={loading} style={{ textDecoration: 'none' }}>
-          {loading ? 'Generating Embeddings...' : 'Generate Embeddings'}
-        </Button>
-      </Col>
-      <Col>
-        {complete ? <div className="square" style={{ backgroundColor: 'lightgreen' }} /> : <div className="square" style={{ backgroundColor: 'red' }} />}
-      </Col>
-    </Row>
+    <Button variant="secondary" onClick={handleGenerateEmbeddings} disabled={loading} style={{ textDecoration: 'none' }}>
+      {loading ? 'Generating Embeddings...' : 'Generate Embeddings'}
+    </Button>
   );
 };
 
