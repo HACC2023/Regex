c5302ba3abda15895110e089c8753f23b9d73d2b
Devin Imamura <143657689+ImamuraDev@users.noreply.github.com>
2023-11-16 21:04:41 -1000

    added slight documentation

--- app/imports/ui/components/StatusSquare.jsx
+++ app/imports/ui/components/StatusSquare.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// CURRENTLY MUST REFRESH TO DISPLAY CHANGES
 /** A square to display current status */
 const StatusSquare = ({ complete }) => (
   complete ? <div className="square" style={{ backgroundColor: 'lightgreen' }}>&#x2713;</div> : <div className="square" style={{ backgroundColor: 'red' }}>X</div>
